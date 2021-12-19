19b8651c8600b9b12aa03972ecfaace84ff1d32d
JungWhoNam <jungwhonam@gmail.com>
2021-12-19 11:11:53 +0900

    Test DOM manipulation

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 
-const data = [
-    { id: 'd1', value: 10, region: 'USA' },
-    { id: 'd2', value: 15, region: 'France' },
-    { id: 'd3', value: 6, region: 'Korea' }
-]
 
-d3.select('div')
-    .selectAll('p')
+const data = [100, "a200", "a300", 400];
+
+for (let i = 0; i < data.length; i++) {
+    // document.body.append(
+    //     document.createElement("p")
+    // );
+    d3.select("body").append("p");
+}
+
+d3.select("body")
+    .selectAll("p")
     .data(data)
-    .enter()
-    .append('p')
-    .text(dta => dta.region)
-
-console.log(d3.select('div').select('p').innerHtml)
+    .text(function (d, i) {
+        console.log("d:", d); // data element
+        console.log("i:", i); // index element
+        console.log("this:", this); // DOM element
+        return `${i}: ${d}`;
+    })
+    .style("color", function(d, i) {
+        var text = this.innerText;
+        if (text.indexOf("a") >= 0) return "red";
+        return "green";
+    });
