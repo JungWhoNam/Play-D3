04a06552326d6dcd440ccfa6b1c95f017e4bb254
JungWhoNam <jungwhonam@gmail.com>
2021-12-22 12:40:23 +0900

    Integrates the range of data and handle the small bar width cases

--- BarChart.js
+++ BarChart.js
@@ -1,6 +1,14 @@
 
-var data = [5, 10, 12];
+var data = [0, 22, 200, 300, 400];
 var width = 200, scaleFactor = 10, barHeight = 20;
+
+var scale = d3.scaleLinear()
+            .domain([d3.min(data), d3.max(data)])
+            .range([0, width]);
+var getWidthInPx = (str) => 
+    str.toString().length * 7;
+var isLeftAligned = (d) => 
+    scale(d) < getWidthInPx(scale(d).toString());
 
 var svg = d3.select("body")
             .append("svg")
@@ -14,12 +22,19 @@ var bar = svg.selectAll("g")
             .attr("transform", (d, i) => `translate(0, ${i * barHeight})`);
 
 bar.append("rect")
-    .attr("width", (d, i) => d * scaleFactor)
+    .attr("width", (d, i) => scale(d))
     .attr("height", barHeight - 1);
 
 bar.append("text")
     .text((d, i) => d)
-    .attr("x", (d, i) => d * scaleFactor)
+    .attr("style", (d) => 
+        isLeftAligned(d) ? "fill: rgb(150,150,150)" : "fill: rgb(255,255,255)"
+    )
+    .attr("x", (d, i) => 
+        isLeftAligned(d) ? getWidthInPx(scale(d).toString()) : scale(d)
+    )
     .attr("y", (barHeight - 1) * 0.5)
-    .attr("dx", "-.35em")
+    .attr("dx", (d) => 
+        isLeftAligned(d) ? ".35em" : "-.35em"
+    )
     .attr("dy", ".35em");
